615b50227ef76a40e7b8f8a6d26b98c611c818ff
Konrad Baumann <baumannkonrad@outlook.de>
2023-12-02 15:08:18 -0500

    imports new dataset and removes collumns in dataframe

--- index.py
+++ index.py
@@ -1,5 +1,15 @@
 import pandas as pd
 
-df = pd.read_csv('./data/Papua New Guinea Weather Data.csv')
+# Imports Puapa New Guinea Weather Data and safes it as a Dataframe
+df = pd.read_csv('./data/Papua New Guinea Weather Data 2022.csv')
 
-print(df['solarenergy'].mean())
+# Drops Rows that are not part of the analysis
+df = df.drop(columns=['name', 'stations', 'uvindex', 'visibility', 'sealevelpressure', 'winddir', 'windspeed', 'windgust', 'preciptype', 'snowdepth', 'snow', 'precipprob', 'precip', 'dew', 'humidity', 'feelslike', 'feelslikemax', 'feelslikemin', 'severerisk'], axis=1)
+
+# Creates Collumn hoursofsunlight that stores the hours between sunrise and sunset rounded to the hour
+
+hoursofsunlight = []
+
+for sunrise, sunset in zip(df['sunrise'], df['sunset']):
+    print(sunrise)
+    print(sunset)
